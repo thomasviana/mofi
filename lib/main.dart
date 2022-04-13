80afbbb280004ec2962a6311ee63175f4adfffab
Thomas Viana <thomasviana16@gmail.com>
2022-04-13 16:24:34 -0500

    Setup firebase options.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,7 +16,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('building');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
