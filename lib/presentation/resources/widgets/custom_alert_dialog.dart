be2d472aa83ee1745e5c1fcd70e2005f867078d1
Thomas Viana <thomasviana16@gmail.com>
2022-04-16 10:04:07 -0500

    Create custom alert dialog widget.

---
+++ lib/presentation/resources/widgets/custom_alert_dialog.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../resources.dart';
+
+class CustomAlertDialog extends StatelessWidget {
+  final String title;
+  final String content;
+  final List<Widget> actions;
+
+  const CustomAlertDialog({
+    Key? key,
+    required this.title,
+    required this.content,
+    required this.actions,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      insetPadding: const EdgeInsets.all(60),
+      contentPadding: EdgeInsets.only(
+        top: 16,
+        left: 8,
+        right: 8,
+        bottom: 0,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(16),
+        ),
+      ),
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 0.0,
+              bottom: 30.0,
+              left: 8.0,
+              right: 8.0,
+            ),
+            child: Text(
+              content,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: AppColors.greySecondary),
+            ),
+          ),
+          ListView.separated(
+            shrinkWrap: true,
+            itemBuilder: ((context, index) => actions[index]),
+            separatorBuilder: (context, index) => const Divider(height: 0.0),
+            itemCount: actions.length,
+          )
+        ],
+      ),
+    );
+  }
+}
