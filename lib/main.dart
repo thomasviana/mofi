d0153766750474c99fdc4f4e558dae1005e9f3d3
Thomas Viana <thomasviana16@gmail.com>
2022-04-21 14:59:29 -0500

    Remove forcing textScaleFactor.

--- lib/main.dart
+++ lib/main.dart
@@ -71,22 +71,6 @@ class _MyAppState extends State<MyApp> {
           Locale('en', ''), // English, no country code
           Locale('es', ''), // Spanish, no country code
         ],
-        builder: (context, child) {
-          // This custom MediaQuery forces the app to ignore any
-          // font-size set by the system to avoid errors with texts
-          // getting clipped out or not rendered appropriately.
-          //
-          // This is by no intention a good solution and its only
-          // purpose is to serve as a temporary fix.
-          //
-          // Additional design research and testing needs to be done
-          // in order to find a proper layout scalability based on
-          // font-size.
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-            child: child!,
-          );
-        },
         theme: AppTheme.light,
         home: WillPopScope(
           child: Navigator(
