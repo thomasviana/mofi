969858944aa740d69161a66cd8a9382dfcc5924c
Thomas Viana <thomasviana16@gmail.com>
2022-04-21 15:00:00 -0500

    Implement persmission handler on pick image.

--- lib/core/user/src/application/pick_user_image.dart
+++ lib/core/user/src/application/pick_user_image.dart
@@ -1,6 +1,9 @@
+import 'dart:io' show Platform;
+
 import 'package:dartz/dartz.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 @injectable
 class PickUserImage {
@@ -10,8 +13,32 @@ class PickUserImage {
   );
 
   Future<Option<XFile>> call() async {
-    final imageFile = await _imagePicker.pickImage(source: ImageSource.gallery);
-    if (imageFile != null) return some(imageFile);
-    return none();
+    if (await _checkPermissions()) {
+      final imageFile =
+          await _imagePicker.pickImage(source: ImageSource.gallery);
+      if (imageFile != null) return some(imageFile);
+      return none();
+    } else {
+      return none();
+    }
+  }
+
+  Future<bool> _checkPermissions() async {
+    if (Platform.isIOS) {
+      final photosStatus = await Permission.photos.status;
+      if (photosStatus.isDenied) {
+        final photoStatus = await Permission.photos.request();
+        return photoStatus.isGranted;
+      } else {
+        return true;
+      }
+    } else {
+      if (await Permission.storage.status.isDenied) {
+        final storageStatus = await Permission.storage.request();
+        return storageStatus.isGranted;
+      } else {
+        return true;
+      }
+    }
   }
 }
