49fb3762ee9ccfb61ea18acc98ff187c92a3f48d
Thomas Viana <thomasviana16@gmail.com>
2022-06-21 22:20:25 -0500

    Fix typo on accounts remote data source.

--- lib/core/accounts/src/infrastructure/datasources/remote/accounts_remote_data_source.dart
+++ lib/core/accounts/src/infrastructure/datasources/remote/accounts_remote_data_source.dart
@@ -7,10 +7,10 @@ import '../../../../domain.dart';
 import '../../../../infrastructure.dart';
 
 abstract class AccountsRemoteDataSource {
-  Future<void> addOrUpdateAccount(Account category);
+  Future<void> addOrUpdateAccount(Account account);
   Future<void> addOrUpdateAccounts(List<Account> accounts);
   Stream<Option<List<Account>>> getAllAccounts();
-  Future<void> deleteAccount(AccountId categoryId);
+  Future<void> deleteAccount(AccountId accountId);
   Future<void> deleteAllAccounts();
 }
 
@@ -23,13 +23,13 @@ class AccountsRemoteDataSourceImpl implements AccountsRemoteDataSource {
   );
 
   @override
-  Future<void> addOrUpdateAccount(Account category) =>
-      _accountsFirebaseProvider.addOrUpdateAccount(category);
+  Future<void> addOrUpdateAccount(Account account) =>
+      _accountsFirebaseProvider.addOrUpdateAccount(account);
 
   @override
   Future<void> addOrUpdateAccounts(List<Account> accounts) async {
-    for (final category in accounts) {
-      await _accountsFirebaseProvider.addOrUpdateAccount(category);
+    for (final account in accounts) {
+      await _accountsFirebaseProvider.addOrUpdateAccount(account);
     }
   }
 
@@ -38,8 +38,8 @@ class AccountsRemoteDataSourceImpl implements AccountsRemoteDataSource {
       _accountsFirebaseProvider.getAccounts();
 
   @override
-  Future<void> deleteAccount(AccountId categoryId) =>
-      _accountsFirebaseProvider.deleteAccount(categoryId);
+  Future<void> deleteAccount(AccountId accountId) =>
+      _accountsFirebaseProvider.deleteAccount(accountId);
 
   @override
   Future<void> deleteAllAccounts() =>
