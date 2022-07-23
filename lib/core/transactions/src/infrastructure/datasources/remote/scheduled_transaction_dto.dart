c3ba5ec53a838a77463e105cdf4ae075db17a4f1
Thomas Viana <thomasviana16@gmail.com>
2022-07-23 10:50:45 -0500

    Handle nullabillity on scheduled transacion dto budget management.

--- lib/core/transactions/src/infrastructure/datasources/remote/scheduled_transaction_dto.dart
+++ lib/core/transactions/src/infrastructure/datasources/remote/scheduled_transaction_dto.dart
@@ -118,8 +118,9 @@ class ScheduledTransactionDto {
       txBudgetId: data['txBudgetId']! as String,
       incomeType: data['incomeType'] as int?,
       isIncomeManaged: data['isIncomeManaged']! as bool,
-      budgetManagement: (data['budgetManagement']! as Map<String, dynamic>)
-          .map((key, value) => MapEntry(key, value as double)),
+      budgetManagement:
+          ((data['budgetManagement'] ?? {}) as Map<String, dynamic>)
+              .map((key, value) => MapEntry(key, value as double)),
       dueDate: (data['dueDate']! as Timestamp).toDate(),
     );
   }
